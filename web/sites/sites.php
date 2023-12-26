0e4eb940cebb2aa30485119e9998fb7d076a2782
Chuyen Luu <chuyen.luu@ffw.com>
2023-12-26 10:23:23 +0700

    RXP: Update pantheon domains.

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -3,3 +3,7 @@
 $sites['s1.rxp.lndo.site'] = 's1.rxp';
 $sites['s2.rxp.lndo.site'] = 's2.rxp';
 $sites['s3.rxp.lndo.site'] = 's3.rxp';
+
+$sites['s1-rxp.pantheonsite.io'] = 's1.rxp';
+$sites['s2-rxp.pantheonsite.io'] = 's2.rxp';
+$sites['s3-rxp.pantheonsite.io'] = 's3.rxp';
